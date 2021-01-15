37aab4f86d99431a2ce2ddd8e13efbce72732c9b
Stephenxu000 <1296064921@qq.com>
2021-01-16 00:00:09 +0800

    test

---
+++ Sorting_Algorithm/Heap_Sort/Heap_Sort.cpp
@@ -0,0 +1,93 @@
+//6. 堆排序（Heap Sort）
+/*
+利用堆设计的排序算法。
+堆是一个近似完全二叉树的结构，且满足子节点键值总是小于/大于它的父节点
+6.1算法描述
+    1.找min/max下标,两层循环
+    2.外层循环i从0开始，表示已排序元素
+    3.内层循环在[i+1,n]区间找最值，swap(nums[min],nums[i])
+6.2
+    最好 ||平均 || 最坏 时间复杂度O(nlogn) //以2为底 
+    不稳定排序算法
+6.3
+    大根堆：根结点对应元素最大（小根堆即最小）
+
+*/
+#include <vector>
+#include <iostream>
+using namespace std;
+//打印函数
+void print_vector(vector<int> a)
+{
+    for (int i = 0; i < a.size(); i++)
+    {
+        cout << "a[" << i << "]=" << a[i] << " ";
+    }
+    cout << endl;
+}
+//数组初始化
+void init_vector(vector<int> &a)
+{
+    a.push_back(7);
+    a.push_back(9);
+    a.push_back(2);
+    a.push_back(16);
+    a.push_back(8);
+    a.push_back(5);
+    a.push_back(12);
+    a.push_back(13);
+    a.push_back(14);
+} //7 9 2 16 8 5 12 13 14
+
+void rebuildheap(vector<int> &nums, int root, int n) //重建堆
+{
+    //1.初始化：i指向树根
+    int i = root, lchild, maxindex = root;
+    while (i <= n / 2 - 1)
+    {
+        lchild = 2 * i + 1; //左子节点 左+1为右子节点   eg.root：0 左1 右2
+        if (lchild < n - 1 && nums[lchild] < nums[lchild + 1])
+        {
+            maxindex = lchild + 1; //右子结点大，记录大的下标
+        }
+        else
+        {
+            maxindex = lchild;
+        }
+        //nums[maxindex]是nums[i]具有较大关键词的子结点
+        if (nums[maxindex] > nums[i])
+        {
+            swap(nums[maxindex], nums[i]);
+            i = maxindex; //继续重建堆
+        }
+        else
+        {
+            i = n; //终止循环
+        }
+    }
+}
+void HeapSort(vector<int> &nums)
+{
+    //1.初始建堆
+    int i;
+    for (i = nums.size() / 2 - 1; i >= 0; i--)
+    {
+        rebuildheap(nums, i, nums.size());
+    }
+    //2.排序
+    for (i = nums.size() - 1; i > 0; i--)
+    {
+        swap(nums[0], nums[i]);
+        rebuildheap(nums, 0, i);
+    }
+}
+int main()
+{
+    vector<int> a;
+    init_vector(a);
+    cout << "nums to be sorted :" << endl;
+    print_vector(a);
+    cout << "Heap Sort :" << endl;
+    HeapSort(a);
+    print_vector(a);
+}
